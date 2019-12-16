ff32059ecc741dd599bafc1720094259f607ad6e
wstnmssr <38819883+wstnmssr@users.noreply.github.com>
2019-12-16 15:20:53 -0700

    Update MonteCarlo.py

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -4,11 +4,11 @@ from collections import Counter
 
 class MonteCarloAI:
     
-    game = GameEngine(white_team_file, black_team_file, output_file)
+    #game = GameEngine(white_team_file, black_team_file, output_file)
     
-    def __init__(self, color):
-        self.color = color
-        self.opponentColor = 'W' if self.color == 'B' else 'B'
+    def __init__(self, color, n, time_limit):
+        self.team_type = color
+        self.opponentColor = 'W' if color == 'B' else 'B'
     
     def get_team_name(self):
         return "team Team"
@@ -25,7 +25,7 @@ class MonteCarloAI:
     def get_move(self, board):
         counter = Counter()
         
-        possibleMoves = game.get_all_moves(board, self.color)
+        possibleMoves = OthelloEngine.get_all_moves(board, self.color)
         coordinates = []
         for move in possibleMoves:
             coordinates.append(move[1])
@@ -33,7 +33,7 @@ class MonteCarloAI:
         for moves in randomMove:
             for i in range(100):
                 tempBoard = deepcopy(board)
-                game.update_board(moves)
+                #game.update_board(moves)
                 if winner == self.color:
                     counter[moves] += 1
         nextMove = counter.most_common(1)[0][0] if len(counter) > 0 else random.choice(moves)
